55520fd53f31dc71be2819a84e5ba4c1e3a34c3e
hoainv1807 <47892115+hoainv1807@users.noreply.github.com>
2025-05-01 16:06:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-docker run -d -p 5901:5901 -p 6080:6080 -p 22222:22222 \
+docker run -d -p 5001:5901 -p 4001:22222 \
   -e VNC_PASSWORD=password \
-  -e VNC_RESOLUTION=1600x900 \
-  -e P2P_EMAIL=email@example.com \
   -v /etc/_docker/ubuntu-xfce-vnc:/root/.local/share \
   ubuntu-xfce-vnc
