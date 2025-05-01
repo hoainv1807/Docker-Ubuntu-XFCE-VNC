ce78df47601fc7c8ebb553fcc97a624f610a2ae6
hoainv1807 <47892115+hoainv1807@users.noreply.github.com>
2025-05-01 19:07:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-docker run -d -p 5001:5901 -p 4001:22222 \
+docker run -d --name Ubuntu \
+  -p 5001:5901 -p 4001:22222 \
   -e VNC_PASSWORD=password \
   -v /etc/_docker/ubuntu-xfce-vnc:/root/.local/share \
   ubuntu-xfce-vnc
